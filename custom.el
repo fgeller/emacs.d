3650ed39df8d5931a12ca22aadd7e7849f84c2e2
Felix Geller <fgeller@gmail.com>
2014-11-14 07:36:35 +1300

    Trying a different approach to loading functionality

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("fc81a433d8c8bda581f800d9d6a8da61a74ba5f0c08bcf1fa7ac9661798a67a5" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
