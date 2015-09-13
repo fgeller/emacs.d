506e5e65aaf7f60d3ee361d81c016c14ae2cc29c
Felix Geller <fgeller@gmail.com>
2015-09-14 07:51:02 +1200

    tweaks for a faster emacs startup.
    https://www.reddit.com/r/emacs/comments/3kqt6e/2_easy_little_known_steps_to_speed_up_emacs_start/

--- init.el
+++ init.el
@@ -1,5 +1,7 @@
 ; -*- coding:utf-8 -*-
 
+(setq gc-cons-threshold 100000000)
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/org"))
-(require 'org)
-(org-babel-load-file (expand-file-name "~/.emacs.d/main.org"))
+(let ((file-name-handler-alist nil)) 
+  (require 'org)
+  (org-babel-load-file (expand-file-name "~/.emacs.d/main.org")))
