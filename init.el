c17be8bbb21e6880a113239071b5e8beeb35bb3e
Felix Geller <fgeller@gmail.com>
2015-09-17 22:47:41 +1200

    adds comments for intention of these hacks.

--- init.el
+++ init.el
@@ -1,8 +1,14 @@
 ; -*- coding:utf-8 -*-
 
+;; don't gc during startup
 (setq gc-cons-threshold 100000000)
+
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/org"))
-(let ((file-name-handler-alist nil)) 
+
+;; temporarily ignore tramp and other regex'd handlers. got to love dyn scope.
+(let ((file-name-handler-alist nil))
   (require 'org)
   (org-babel-load-file (expand-file-name "~/.emacs.d/main.org")))
+
+;; reduce gc threshold to decrease gc pause
 (setq gc-cons-threshold 1000000)
