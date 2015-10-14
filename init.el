351d81fe1be3ec17f3693e6f69136eeaae730ebb
Felix Geller <fgeller@gmail.com>
2015-10-14 23:36:41 +1300

    Simplifies temporary customizations during startup.

--- init.el
+++ init.el
@@ -1,14 +1,9 @@
 ; -*- coding:utf-8 -*-
 
-;; don't gc during startup
-(setq gc-cons-threshold 100000000)
-
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/org"))
-
-;; temporarily ignore tramp and other regex'd handlers. got to love dyn scope.
-(let ((file-name-handler-alist nil))
+(let (;; don't gc during startup
+      (gc-cons-threshold 100000000)
+      ;; temporarily ignore tramp and other regex'd handlers.
+      (file-name-handler-alist nil))
+  (add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/org"))
   (require 'org)
   (org-babel-load-file (expand-file-name "~/.emacs.d/main.org")))
-
-;; reduce gc threshold to decrease gc pause
-(setq gc-cons-threshold 1000000)
