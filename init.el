1fde930fffa4afb3d68e9f0a7169cfa2b75eb065
Felix Geller <fgeller@gmail.com>
2015-10-24 12:08:53 +1300

    whitespace.

--- init.el
+++ init.el
@@ -1,5 +1,8 @@
 ; -*- coding:utf-8 -*-
 
+; package.el forces this line to exist
+; (package-initialize)
+
 (let (;; don't gc during startup
       (gc-cons-threshold 100000000)
       ;; temporarily ignore tramp and other regex'd handlers.
